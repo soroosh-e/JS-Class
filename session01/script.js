824b3f88883d6f4731d7a12ca521b6067da366eb
Soroosh <soroosh.e@gmail.com>
2024-10-03 17:31:34 +0330

    first commit

---
+++ session01/script.js
@@ -0,0 +1,42 @@
+var firstName = "John";
+var lastName = "Wick";
+var age = 40;
+var light = true
+var aaa = null
+
+//Variable Types
+/*
+1. Number
+2. String
+3. Boolean (true/false)
+4. Undefined
+5. Null
+*/
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+
+console.log(firstName, lastName, age)
+
+console.log(typeof(aaa))
+
+//Variable Naming Rules
+//var 5name = 40
+var $abc 
+var _df
+
+console.log(_df)
+
+var a = 1; var b = 5; var c = 6
+var x = 1,y = 2,z = 3
+
+console.log(z)
+
+//Variable Mutation
+age = 50
+console.log(age)
+
+console.log(firstName + ' ' + lastName + ' is ' + age + ' years old.')
+
+alert(firstName + ' ' + lastName + ' is ' + age + ' years old.')
---
+++ session02/script.js
@@ -0,0 +1,28 @@
+//prompt
+// var firstName = prompt('What is your name?');
+
+// console.log('your name is: ' + firstName)
+
+//Operators
+//Math Operators
+//+  - / * ^
+var a = 5
+var b = 1
+console.log(a - b)
+
+//Comparison Operators
+// >  <   == === >= <= !=
+
+// console.log(a > b)
+
+var x = 5
+var y = 5
+
+// console.log(x <= y)
+
+//Other Operators
+console.log(x += 2) //x = x + 2
+console.log(x *= 2) //x = x * 2
+console.log(x++)
+console.log(++x)
+console.log(x)
